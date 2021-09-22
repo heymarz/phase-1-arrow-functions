d27bef9dc83977b4e26b6dbc8ae4871aa15b1f06
Marlena Tsang <Marlena345@gmail.com>
2021-09-22 10:44:00 -0400

    commit tree 93885ca40e1cf39f8062aac2e8f74d1a44417135

---
+++ index.js
@@ -0,0 +1,8 @@
+//has fx expression called divide
+const divide = x => (2000 / 100)
+
+//has arrow fx called square
+const square = x => x ** 2;
+
+//has arrow fx called add
+const add = (a,b) => (a+b);
